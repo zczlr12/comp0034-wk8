39cb01c295a97c2e7b4922cd7470c2cfc0b5529b
nicholsons <sarah.sanders@ucl.ac.uk>
2024-01-16 17:05:02 +0000

    update images and flask app

--- src/paralympics_flask/views.py
+++ src/paralympics_flask/views.py
@@ -1,46 +1,20 @@
-from flask import current_app as app, render_template
+from flask import current_app as app, render_template, url_for
+
+from paralympics_flask import db
+from paralympics_flask.models import Event
 
 
-# COMPLETED EXAMPLES OF WEEK 6 ACTIVITIES
-# You can delete these once you've compared them to your work
-@app.route('/html', methods=['GET'])
-def index_html():
-    """
-    Returns a view using a basic HTML template with no CSS or Jinja.
-    """
-    return render_template('index_html.html')
-
-
-@app.route('/css', methods=['GET'])
-def index_css():
-    """
-    Returns a view using a basic HTML template with Bootstrap CSS.
-    """
-    return render_template('index_css.html')
-
-
-@app.route('/responsive', methods=['GET'])
-def index_responsive():
-    """
-    Returns a view using Bootstrap CSS and defines the viewport.
-    """
-    return render_template('index_responsive.html')
-
-
-@app.route('/jinja', methods=['GET'])
-def index_jinja():
-    """
-    Returns a view using a child Jinja template with Bootstrap CSS.
-    """
-    return render_template('index_jinja.html')
-
-
-# STARTER CODE FOR ACTIVITY 7
 @app.route('/', methods=['GET'])
 def index():
-    """
-    Returns the home page.
+    """ Returns the home page."""
+    # Query the database to get all the event
+    # events = db.session.execute(db.select(Event.id, Event.host, Event.year)).scalars()
+    events = db.session.execute(db.select(Event).order_by(Event.year)).scalars()
+    return render_template('index.html', events=events)
 
-     Use as the starting point for week 7 activities.
-    """
-    return render_template('index.html')
+
+@app.get('/events/<event_id>')
+def get_event(event_id):
+    """ Returns an event details page. """
+    event = db.get_or_404(Event, event_id)
+    return render_template('event.html', event=event)
